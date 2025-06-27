8fed981aba62bf8cf71fd1a6309db9b638e0ffcb
Mangellch <mariangelaraviches@gmail.com>
2025-06-27 13:28:46 -0400

    add os enviroment on app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask, request
 from config import Config
-from extensions import db, mail, migrate     
+from extensions import db, mail, migrate
 from notes.routes import notes_bp
 from auth.routes import auth_bp
 
@@ -30,4 +31,11 @@ def create_app(config_class=Config):
             return "Form submitted successfully!", 201
         return "This is the contact page."
 
+    with app.app_context():
+        db.create_all()  # Crea las tablas si no existen
+
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
